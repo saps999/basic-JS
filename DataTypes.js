819441b0e1bb71919f9f3d57b32066d525be6044
saps999 <saptarshibiswasgang@gmail.com>
2023-01-18 12:57:57 +0530

    Data Types Example

---
+++ DataTypes.js
@@ -0,0 +1,34 @@
+// DATA TYPES
+
+// 1. Numbers
+let num = 123;
+
+// 2. Strings
+let name= "Saptarshi";
+
+// 3. Boolean
+let a = true;
+let b = false;
+
+// 4. Arrays
+let arr = [1,2,"car",true]; // we can store multiple types of elements in array
+console.log(arr);
+
+// 5. Object
+let date = new Date("2023-01-18");
+console.log(date);
+
+// 6. Functions
+function add(a,b){
+    console.log(a+b);
+}
+
+add(2,3);
+
+// 7. Undefined
+let x;
+console.log(x);
+
+// 8. Null
+let y =null;
+console.log(y);
