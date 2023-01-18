5e9a96bb3ae339c4f9ca5b90e1dd7b290e17d40a
saps999 <saptarshibiswasgang@gmail.com>
2023-01-18 13:42:39 +0530

    Variable Declaration Example

---
+++ VariableDeclaration.js
@@ -0,0 +1,19 @@
+// 1. var
+// var a =10;
+// console.log(a)
+// var a =20;
+// console.log(a)
+
+// 2. let
+// let a = 10;
+// console.log(a)
+// let a = 20 //SyntaxError: 'a' already declared
+// a =20;
+// console.log(a)
+
+// 3. const
+const a = 10;
+console.log(a)
+
+a = 20; //TypeError: Assignment to constant variable.
+console.log(a)
