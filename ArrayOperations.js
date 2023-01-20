b641395ee8c94d2e96ae422356cbf7de784a02bc
saps999 <saptarshibiswasgang@gmail.com>
2023-01-20 22:22:58 +0530

    [Saptarshi]Added: Array Operations Examples

---
+++ ArrayOperations.js
@@ -0,0 +1,45 @@
+// let numbers = [0,1,2,3,4,5,6,7,8,9,10];
+// 1. printing the array
+// console.log(numbers)
+
+// 2. push method adds elements in the array at the last index
+// numbers.push(11)
+// console.log(numbers)
+
+// 3. pop method removes one elements in the array from the last index
+// numbers.pop();
+// console.log(numbers)
+
+// 4. sorting method
+// let numbers1 = [1,6,5,2,8,9,4,3,7];
+// numbers1.sort();
+// console.log(numbers1)
+
+// 5. foreach loop
+// numbers1.forEach((value,index)=>{
+//     console.log(value,index)
+// })
+
+// 6. splice method
+// let car =["BMW","Mercedes-Benz","KIA"];
+// car.splice(2,0,"Bugatti")
+// console.log(car)
+
+// car.splice(1,1,"Bugatti")
+// console.log(car)
+
+// car.splice(1,1,"Bugatti","RR","Maserati")
+// console.log(car)
+// car.splice(1,0,"Bugatti","RR","Maserati")
+// console.log(car)
+
+// 7. split method
+// let text="hello world"
+// let arr=text.split(" ")
+// console.log(arr)
+
+// 8. slice methods
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+console.log(fruits)
+const fruit1 = fruits.slice(1, 3);
+console.log(fruit1)
